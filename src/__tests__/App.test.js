44cc98f9e2224057459123cf062773b60817db7a
itskheila <munenetiara@gmail.com>
2025-07-26 14:18:53 +0300

    commit

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -6,6 +6,7 @@ import {
   screen,
   waitForElementToBeRemoved,
 } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
 import { server } from "../mocks/server";
 
 import App from "../components/App";
